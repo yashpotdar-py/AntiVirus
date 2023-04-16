275deab19f0bebab1aff0494a7dcf66f71ad0652
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-04-16 22:05:01 +0530

    first commit

---
+++ gui/main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
+root = Tk()
+
+root.geometry("900x600+10+10")
+root.maxsize(900, 600)
+root.minsize(900, 600)
+
+root.title("AntiVirus Program in Python")
+
+
+root.mainloop()

53c15cd7f9f51acf7b6692cdd921ab3238cc072b
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-04-16 23:13:13 +0530

    Adding malware detection

---
+++ main/hash.py
@@ -0,0 +1,27 @@
+import hashlib
+
+def md5_hash(file_name):
+    with open(file_name, 'rb') as f:
+        bytes = f.read()
+        md5hash = hashlib.md5(bytes).hexdigest()
+        f.close()
+    
+    return md5hash
+
+print(md5_hash("gui\main.py"))
+
+
+def check_hash(file_path):
+    file_hash = md5_hash(file_path)
+    counter=0
+    malware_hash = list(open("main\hash_key.txt", 'r').read().split("\n"))
+    malware_name = list(open("main\hash_name.txt", 'r').read().split("\n"))
+    
+    for i in malware_hash: 
+        if i == file_hash:
+            return malware_name[counter]
+        counter+=1
+    return "No Malware"
+        
+   
+print(check_hash("gui\main.py"))

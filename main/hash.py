cfa73fdd43116917a8a5412b39caf48c082db643
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-04-17 17:54:31 +0530

    Added hash_name.txt.Updated hash_key.txt with more SHA256 hashes Modified hash_key.txt and hash.py by changing from MD5 to SHA256.

--- main/hash.py
+++ main/hash.py
@@ -1,9 +1,11 @@
 import hashlib
 
+malware_hash = list(open("main\hash_key.txt", 'r').read().split("\n"))
+malware_name = list(open("main\hash_name.txt", 'r').read().split("\n"))
 def md5_hash(file_name):
     with open(file_name, 'rb') as f:
         bytes = f.read()
-        md5hash = hashlib.md5(bytes).hexdigest()
+        md5hash = hashlib.sha256(bytes).hexdigest()
         f.close()
     
     return md5hash
@@ -13,13 +15,13 @@ print(md5_hash("gui\main.py"))
 
 def check_hash(file_path):
     file_hash = md5_hash(file_path)
-    counter=0
-    malware_hash = list(open("main\hash_key.txt", 'r').read().split("\n"))
-    
+    global malware_hash
+    counter = 0
     for i in malware_hash: 
         if i == file_hash:
-            return "Malware Detected"
-        counter+=1
+            print("MALWARE DETECTED")
+            return malware_name[counter]
+        counter += 1
     return "No Malware"
         
    
